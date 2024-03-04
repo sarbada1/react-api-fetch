3c2bd3e1666cd307a32b5626de35c15cc76e31ca
sarbada1 <sanjelsarbada12@gmail.com>
2024-03-04 07:27:18 +0545

    react api fetch

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function App(){
+  return(
+    <div className='container'>
+      <h1>CRUD</h1>
+
+    </div>
+  )
+}
